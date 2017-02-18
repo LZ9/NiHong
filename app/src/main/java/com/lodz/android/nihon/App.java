ee9d64f1e01dcf8ffe0df74e5d2692a613498985
lodz <lz.mms@foxmail.com>
2017-02-18 20:53:09 +0800

    Initial commit

---
+++ app/src/main/java/com/lodz/android/nihon/App.java
@@ -0,0 +1,20 @@
+package com.lodz.android.nihon;
+
+import com.lodz.android.component.base.BaseApplication;
+
+/**
+ * Application
+ * Created by zhouL on 2017/2/18.
+ */
+public class App extends BaseApplication{
+
+    @Override
+    protected void afterCreate() {
+
+    }
+
+    @Override
+    protected void beforeExit() {
+
+    }
+}
---
+++ app/src/main/java/com/lodz/android/nihon/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lodz.android.nihon.ui;
+
+import android.os.Bundle;
+
+import com.lodz.android.component.base.BaseActivity;
+import com.lodz.android.nihon.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void findViews(Bundle savedInstanceState) {
+
+    }
+}
